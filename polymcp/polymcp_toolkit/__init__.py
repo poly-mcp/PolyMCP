bf140d8ba7c05f185d5d5c730e68c6d11d8d9af9
Vincenzo <vincenzofornaro89@gmail.com>
2025-12-22 00:29:32 +0100

    Update __init__.py

--- polymcp/polymcp_toolkit/__init__.py
+++ polymcp/polymcp_toolkit/__init__.py
@@ -7,9 +7,18 @@ from .expose import (
     InProcessMCPServer,     # In-process server class
 )
 
+# NEW: Stdio and WASM server modes
+from .expose_tools_stdio import expose_tools_stdio
+from .expose_tools_wasm import expose_tools_wasm
+
 __all__ = [
+    # HTTP/In-process
     'expose_tools',
     'expose_tools_http', 
     'expose_tools_inprocess',
     'InProcessMCPServer',
+    
+    # NEW: Stdio and WASM
+    'expose_tools_stdio',
+    'expose_tools_wasm',
 ]
